60ac69572cf97e4124678594570a4c2c24014c2f
Kerem Çakırer <kcakirer@hotmail.com>
2020-03-15 23:20:18 +0300

    Add dispatch functions for other requests

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -15,6 +15,7 @@
 
 use std::error::Error;
 
+use hyper::http::response::Builder;
 use hyper::{Body, Method, Request, Response};
 use serde::{Deserialize, Serialize};
 
@@ -23,14 +24,76 @@ pub type DispatchResult = Result<Response<Body>, DispatchError>;
 
 pub async fn dispatch(req: Request<Body>) -> DispatchResult {
     match (req.method(), req.uri().path()) {
+        (&Method::GET, "/events") => dispatch_events(req).await,
+        (&Method::POST, "/connect") => dispatch_connect(req).await,
         (&Method::POST, "/create") => dispatch_create(req).await,
-        _ => Ok(Response::new("dispatch()".into())),
+        (&Method::POST, "/reconnect") => dispatch_reconnect(req).await,
+        (&Method::POST, "/update") => dispatch_update(req).await,
+        _ => not_found(),
     }
 }
+
+// Handle /events requests
+
+#[derive(Deserialize, Serialize)]
+struct Event {}
+
+async fn dispatch_events(req: Request<Body>) -> DispatchResult {
+    Ok(Response::new("dispatch_events()".into()))
+}
+
+// Handle /connect requests
+
+#[derive(Deserialize, Serialize)]
+struct ConnectReq {}
+
+#[derive(Deserialize, Serialize)]
+struct ConnectResp {}
+
+async fn dispatch_connect(req: Request<Body>) -> DispatchResult {
+    Ok(Response::new("dispatch_connect()".into()))
+}
+
+// Handle /create requests
 
 #[derive(Deserialize, Serialize)]
 struct CreateResp {}
 
 async fn dispatch_create(req: Request<Body>) -> DispatchResult {
     Ok(Response::new("dispatch_create()".into()))
+}
+
+// Handle /reconnect requests
+
+#[derive(Deserialize, Serialize)]
+struct ReconnectReq {}
+
+#[derive(Deserialize, Serialize)]
+struct ReconnectResp {}
+
+async fn dispatch_reconnect(req: Request<Body>) -> DispatchResult {
+    Ok(Response::new("dispatch_reconnect()".into()))
+}
+
+// Handle /update requests
+
+#[derive(Deserialize, Serialize)]
+struct UpdateReq {}
+
+#[derive(Deserialize, Serialize)]
+struct UpdateResp {}
+
+async fn dispatch_update(req: Request<Body>) -> DispatchResult {
+    Ok(Response::new("dispatch_update()".into()))
+}
+
+// Helper functions
+
+// TODO: Don't set Access-Control-Allow-Origin to *
+fn builder() -> Builder {
+    Response::builder().header("Access-Control-Allow-Origin", "*")
+}
+
+fn not_found() -> DispatchResult {
+    Ok(builder().status(404).body(Body::empty())?)
 }
